00a0f89cc25daeba3552cd27e7e3d33f13285853
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2021-02-09 11:05:12 +0545

    Add stubs for readline and add_history for windows

--- prompt.c
+++ prompt.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Stubbing out readline and history for windows
+ * Apparently windows handles history and 
+ * navigation all well by default. */
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char* readline(char* prompt)
+{
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = "\0";
+	return cpy;
+}
+
+void add_history(char* unused) {}
+
+
+#else
 #include <readline/history.h>
 #include <readline/readline.h>
-
+#endif
 
 int main(int argc, char** argv) 
 {
