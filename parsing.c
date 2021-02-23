424bf95b903e7211a33419b0e2cd63142cb7b85b
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2021-02-23 11:20:59 +0545

    Read Q expressions

--- parsing.c
+++ parsing.c
@@ -50,7 +50,7 @@ typedef struct lval
 	struct lval** cell;
 } lval;
 
-enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
+enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR, LVAL_QEXPR };
 
 enum { LERR_DIV_BY_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
@@ -93,6 +93,16 @@ lval* lval_sexpr(void)
 }
 
 
+lval* lval_qexpr(void)
+{
+	lval* v = malloc(sizeof(lval));
+	v->type = LVAL_QEXPR;
+	v->count = 0;
+	v->cell = NULL;
+	return v;
+}
+
+
 void lval_del(lval* v)
 {
 	switch(v->type)
@@ -105,6 +115,7 @@ void lval_del(lval* v)
 		case LVAL_SYM: free(v->sym); break;
 
 		case LVAL_SEXPR:
+		case LVAL_QEXPR:
 			for (int i = 0; i < v->count; i++)
 			{
 				lval_del(v->cell[i]);
@@ -147,6 +158,7 @@ void lval_print(lval* v)
 		case LVAL_ERR: printf("Error: %s", v->err); break;
 		case LVAL_SYM: printf("%s", v->sym); break;
 		case LVAL_SEXPR: lval_expr_print(v, '(', ')');  break;
+		case LVAL_QEXPR: lval_expr_print(v, '{', '}');  break;
 
 	}
 }
@@ -205,11 +217,14 @@ lval* lval_read(mpc_ast_t* t)
 	lval* x = NULL;
 	if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
 	if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+	if (strstr(t->tag, "qexpr")) { x = lval_qexpr(); }
 
 	for (int i = 0; i < t->children_num; i++)
 	{
 		if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
 		if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+		if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+		if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
 		if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
 		x = lval_add(x, lval_read(t->children[i]));
 	}
@@ -343,6 +358,7 @@ int main(int argc, char** argv)
 	mpc_parser_t* Number = mpc_new("number");
 	mpc_parser_t* Symbol = mpc_new("symbol");
 	mpc_parser_t* Sexpr = mpc_new("sexpr");
+	mpc_parser_t* Qexpr = mpc_new("qexpr");
 	mpc_parser_t* Expr = mpc_new("expr");
 	mpc_parser_t* Lisp = mpc_new("lisp");
 
@@ -351,10 +367,11 @@ int main(int argc, char** argv)
 		number	: /-?[0-9]+/ ;				\
 		symbol : '+' | '-' | '*' | '/' ;		\
 		sexpr	: '(' <expr>* ')' ;			\
-		expr	: <number> | <symbol> | <sexpr> ;	\
+		qexpr	: '{' <expr>* '}' ;			\
+		expr	: <number> | <symbol> | <sexpr> | <qexpr> ;	\
 		lisp	: /^/ <expr>* /$/ ;			\
 		",
-		Number, Symbol, Sexpr, Expr, Lisp);
+		Number, Symbol, Sexpr, Qexpr, Expr, Lisp);
 
 	/* Debug info */
 	puts("Lisp version 0.0.0.0.1");
@@ -397,7 +414,7 @@ int main(int argc, char** argv)
 
 	}
 
-	mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lisp);
+	mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Lisp);
 
 	return 0;
 
